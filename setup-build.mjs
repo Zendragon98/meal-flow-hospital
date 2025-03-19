7224ad9b2b4ee5a358b0684a2e155a24b67078ae
Zenith Tay <zenithisaiahtay@gmail.com>
2025-03-19 11:28:06 +0800

    vercel 3.0

---
+++ setup-build.mjs
@@ -0,0 +1,6 @@
+// This file is used by the Vercel build process
+// It performs any necessary setup steps before the build
+console.log('Setup build process started...');
+
+// Since we're using ES modules, we need to explicitly export something
+export default {}; 
